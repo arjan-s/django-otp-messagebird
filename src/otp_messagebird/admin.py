714becbba350d55150bad425b302f5d0185e99bd
Arjan Schrijver <arjan.schrijver@foxcrypto.com>
2020-06-17 11:48:46 +0200

    Register voice device on admin site

--- src/otp_messagebird/admin.py
+++ src/otp_messagebird/admin.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from django.contrib import admin
 from django.contrib.admin.sites import AlreadyRegistered
 
-from .models import MessageBirdSMSDevice
+from .models import MessageBirdSMSDevice, MessageBirdVoiceDevice
 
 
 class MessageBirdSMSDeviceAdmin(admin.ModelAdmin):
@@ -21,6 +21,26 @@ class MessageBirdSMSDeviceAdmin(admin.ModelAdmin):
 
 try:
     admin.site.register(MessageBirdSMSDevice, MessageBirdSMSDeviceAdmin)
+except AlreadyRegistered:
+    # Ignore the useless exception from multiple imports
+    pass
+
+
+class MessageBirdVoiceDeviceAdmin(admin.ModelAdmin):
+    """
+    :class:`~django.contrib.admin.ModelAdmin` for
+    :class:`~otp_messagebird.models.MessageBirdVoiceDevice`.
+    """
+
+    fieldsets = [
+        ("Identity", {"fields": ["user", "name", "confirmed"], }),
+        ("Configuration", {"fields": ["number", "language"], }),
+    ]
+    raw_id_fields = ["user"]
+
+
+try:
+    admin.site.register(MessageBirdVoiceDevice, MessageBirdVoiceDeviceAdmin)
 except AlreadyRegistered:
     # Ignore the useless exception from multiple imports
     pass
